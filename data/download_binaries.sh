57033362a6b9ab53ac42b88873d2943fdb4f0747
Andherson Maeda <maeda.br@gmail.com>
2018-12-04 23:14:41 -0200

    moved binary files to google drive instead of use lfs @maedabr

---
+++ data/download_binaries.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./download_from_gdrive.sh 10anzjy-iRecL-9nwHKcd5fmziVyAtlGU examples.t7
+./download_from_gdrive.sh 1tVC-fpV_gKPF35N4a5JnGVjqe0SKOoZJ model.t7
+./download_from_gdrive.sh 1eug_c5siKr7a5QyqcwPr_owOJ_o1hsZQ vocab.t7
+
